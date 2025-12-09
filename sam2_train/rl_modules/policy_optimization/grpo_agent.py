1a95c738ee086da7d0f5857520325c6037e50711
danh-real <htduyanh210604@gmail.com>
2025-12-09 05:13:03 +0000

    first version grpo

--- sam2_train/rl_modules/policy_optimization/grpo_agent.py
+++ sam2_train/rl_modules/policy_optimization/grpo_agent.py
@@ -3,9 +3,58 @@ import torch
 from torch import optim
 
 from sam2_train.rl_modules.policy_optimization.base_po_agent import (BasePOAgent, BaseFeatureSummarizer, BasePolicyNetwork)
+from sam2_train.rl_modules.rl_components import RLReplayInstance, RLStates
 
+class GRPOReplayInstance(RLReplayInstance):
+    def __init__(
+        self, 
+        frame_idx=None, 
+        state=None, 
+        action=None,
+        reward=None,
+        log_probs=None,
+    ):
+        super().__init__(frame_idx, state, action, None, None, None, reward)
+        self.log_probs = log_probs
+        
+    def get(self):
+        # Call tuple to create a copy
+        return tuple((self.state, self.log_probs, self.action, self.reward, self.done))
+    
+    # Update after
+    def update(self, loss_after):
+        self.loss_after = loss_after
+        
+        loss_diff = self.loss_before - self.loss_after
+        self.reward = self.reward + loss_diff
 
-class PPOAgent(BasePOAgent):
+    def set_done(self, loss_after):
+        self.loss_after = loss_after
+        
+        loss_diff = self.loss_before - self.loss_after
+        self.reward = self.reward + loss_diff
+        self.done = True
+    
+class GRPOGroup:
+    def __init__(self):
+        self.group = []
+        
+    def add_instance(self, instance):
+        self.group.append(instance)
+        
+    def finalize(self):
+        group_rewards = torch.Tensor([ins.reward for ins in self.group])
+        group_mean = group_rewards.mean(dim=0, keepdim=True)
+        group_std  = group_rewards.std(dim=0, keepdim=True)
+        group_rewards = (group_rewards - group_mean) / group_std
+        
+        for i, ins in enumerate(self.group):
+            ins.reward = group_rewards[i]
+    
+    def get_instances(self):
+        return [ins.get() for ins in self.group]
+
+class GRPOAgent(BasePOAgent):
     def __init__(
         self, 
         num_maskmem, 
@@ -46,6 +95,18 @@ class PPOAgent(BasePOAgent):
             lr=policy_lr
         )
         
+        self.await_group = None
+    
+    def init_new_group(self):
+        self.await_group = GRPOGroup()
+    
+    def add_new_instance_to_group(self, **instance_info):
+        self.await_group.add_instance(GRPOReplayInstance(**instance_info))
+        
+    def final_group(self):
+        self.await_group.finalize()
+        self.replay_buffer.extend(self.await_group.get_instances())
+        
     def select_action(self, state, valid_actions, num_samples=1, training=False):
         image_feat = state.next_image_feat
         memory_feat = state.curr_memory_feat["mem_feat"]
@@ -68,6 +129,55 @@ class PPOAgent(BasePOAgent):
             "action": [valid_actions[idx].item() for idx in action_idx],
             "log_probs": [torch.log(valid_probs[idx]) for idx in action_idx]
         }
+    
+    def train_step(self, batch, update_value=True):
+        device = self.device
+        
+        states, old_log_probs, actions, rewards, next_states, dones, returns, advantages = zip(*batch)
+        
+        image_feat = torch.cat([state.next_image_feat for state in states]).to(device=device, non_blocking=True)
+        memory_feat = torch.cat([state.curr_memory_feat["mem_feat"] for state in states]).to(device=device, non_blocking=True)
+        memory_ptr = torch.cat([state.curr_memory_feat["obj_ptr"] for state in states]).to(device=device, non_blocking=True)
+        bank_feat = torch.cat([state.prev_memory_bank["mem_feat"] for state in states]).to(device=device, non_blocking=True)
+        bank_ptr = torch.cat([state.prev_memory_bank["obj_ptr"] for state in states]).to(device=device, non_blocking=True)
+        
+        actions = torch.LongTensor(actions).unsqueeze(1).to(device=device, non_blocking=True)
+        rewards = torch.FloatTensor(rewards).unsqueeze(1).to(device=device, non_blocking=True)
+        old_log_probs = torch.FloatTensor(old_log_probs).unsqueeze(1).to(device=device, non_blocking=True)
+        dones = torch.FloatTensor(dones).unsqueeze(1).to(device=device, non_blocking=True)
+        
+        adv_mean = advantages.mean(dim=0, keepdim=True)
+        adv_std = advantages.std(dim=0, keepdim=True)
+        advantages = 0.7 * (advantages - adv_mean) / adv_std
+        
+        # print("advantages", advantages.mean())
+        
+        with torch.enable_grad():
+            curr_feats = self.feat_summarizer(image_feat, memory_feat, memory_ptr, bank_feat, bank_ptr)
+            
+            policy_probs = self.policy_net(*curr_feats)
+            action_probs = policy_probs.gather(1, actions)
+            log_probs = torch.log(policy_probs)
+            log_action_probs = torch.log(action_probs)
+            
+            policy_loss = self.compute_policy_loss(log_action_probs, advantages, old_log_probs)
+            minus_entropy = (policy_probs * log_probs).sum(dim=1, keepdim=True)
+            policy_loss += minus_entropy * self.entropy_weight # entropy regularization
+            policy_loss = policy_loss.mean()
+            
+            self.policy_optimizer.zero_grad()
+            policy_loss.backward()
+            self.policy_optimizer.step()
+            
+            # total_loss = policy_loss + 0.1 * value_loss
+            
+            # self.optimizer.zero_grad()
+            # total_loss.backward()
+            # self.optimizer.step()
+            
+            # print("loss", policy_loss, value_loss, minus_entropy.mean())
+            
+        return policy_loss.item()
     
     def compute_policy_loss(self, log_prob, advantage, old_log_prob):
         ratio = (log_prob - old_log_prob).exp()
