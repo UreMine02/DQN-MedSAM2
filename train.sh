e804f8c196a6031f3c9a9ca59a2f4967fad0ba3e
danh-real <htduyanh210604@gmail.com>
2025-12-14 21:57:11 +0000

    grpo

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EXP=msd_task02+grpo+entropy1e-1+num_support3+clip_grad0.1
+EXP=msd_task03+grpo+entropy1e-1+num_support3+clip_grad0.1
 
 CUDA_VISIBLE_DEVICES=0 python train_3d.py \
     -exp_name $EXP \
@@ -8,11 +8,11 @@ CUDA_VISIBLE_DEVICES=0 python train_3d.py \
     -rl_config rl_modules/config/grpo_po_agent.yaml \
     -checkpoint_path ./output/$EXP \
     -dataset msd \
-    -task Task02 \
+    -task Task03 \
     -data_path /data/datasets/MSD \
-    -lr 2e-4 \
+    -lr 1e-4 \
     -val_freq 1 \
-    -ep 500 \
+    -ep 100 \
     -q_updates_per_step 2 \
     -lazy_penalty -0.01 \
     -invalid_penalty -0.01 \
--- train_3d.py
+++ train_3d.py
@@ -66,7 +66,7 @@ def train(rank=0, world_size=0):
         net.module.agent.q_net = DDP(net.module.agent.q_net, device_ids=[rank])
     
     optimizer = optim.AdamW(net.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-08, amsgrad=False, fused=True)
-    scheduler = StepLR(optimizer, step_size=100, gamma=0.5)
+    # scheduler = StepLR(optimizer, step_size=100, gamma=0.5)
 
     torch.autocast(device_type="cuda", dtype=torch.bfloat16).__enter__()
 
@@ -99,7 +99,7 @@ def train(rank=0, world_size=0):
             'train/mae_loss': mae_loss, 
             'train/bce_loss': bce_loss, 
             "train/actor_loss": agent_loss["actor_loss"],
-            "train/lr": scheduler.get_last_lr()[0],
+            # "train/lr": scheduler.get_last_lr()[0],
         }
         
         if args.wandb_enabled and loss is not None:
@@ -131,7 +131,7 @@ def train(rank=0, world_size=0):
             if args.wandb_enabled:
                 wandb.log({'val/IOU' : iou, 'val/dice' : dice}, step=epoch)
         
-        scheduler.step()
+        # scheduler.step()
         
         if args.save_ckpt:
             if args.distributed and dist.get_rank() == 0:
