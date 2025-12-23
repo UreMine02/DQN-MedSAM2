62458b61373ea0da964d211cfbaf662a4924a908
danh-real <htduyanh210604@gmail.com>
2025-12-23 22:04:12 +0000

    grpo

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EXP=msd_task02+grpo+entropy1e-1+num_support10+clip_grad0.1
+EXP=sarcoma+grpo+entropy1e-1+num_support10+clip_grad0.1
 export CUDA_VISIBLE_DEVICES=0
 
 python train_3d.py \
@@ -8,17 +8,17 @@ python train_3d.py \
     -sam_ckpt ./checkpoints/sam2_hiera_tiny.pt \
     -rl_config rl_modules/config/grpo_po_agent.yaml \
     -checkpoint_path ./output/$EXP \
-    -dataset msd \
+    -dataset sarcoma \
     -task Task02 \
-    -data_path /data/datasets/MSD \
+    -data_path /data/datasets/Sarcoma \
     -lr 1e-4 \
     -val_freq 1 \
-    -ep 300 \
+    -ep 100 \
     -q_updates_per_step 2 \
     -lazy_penalty -0.01 \
     -invalid_penalty -0.01 \
     -num_support 10 \
-    # -wandb_enabled
+    -wandb_enabled
 
 # CUDA_VISIBLE_DEVICES=0 python train_3d.py \
 #     -exp_name sarcoma+ppo+normalized_gae0.99+entropy1e-1+num_support3 \
