3d92c478e024a65899c49dafe837a7ffcdd20090
danh-real <htduyanh210604@gmail.com>
2026-01-02 02:50:07 +0000

    grpo

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 python eval_3d.py \
-    -pretrain output/msd_task03+grpo+entropy1e-1+num_support10+clip_grad0.1/2025-12-30-10-37-01/best.pth \
+    -pretrain output/hpc/msd_task03+grpo+entropy1e-1+num_support10+clip_grad0.1/2025-12-30-10-37-01/best.pth \
     -rl_config rl_modules/config/grpo_po_agent.yaml \
     -dataset msd \
     -task Task03 \
-    -data_path /hpcfs/users/a1232079/duyanh/MedSAM2/datasets/MSD \
+    -data_path /data/datasets/MSD \
     -num_support 5
