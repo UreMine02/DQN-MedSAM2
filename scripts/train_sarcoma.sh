da3a8f2d8933144fbfe8401d367cdd67bde9ccd4
danh-real <htduyanh210604@gmail.com>
2026-01-06 06:25:06 +0000

    rl

--- scripts/train_sarcoma.sh
+++ scripts/train_sarcoma.sh
@@ -14,7 +14,7 @@
 # conda init
 # conda activate rlsam2
 
-EXP=sarcoma+ppo+perceiver
+EXP=sarcoma+no_agent
 export CUDA_VISIBLE_DEVICES=1
 
 python train_3d.py \
@@ -31,4 +31,5 @@ python train_3d.py \
     -lazy_penalty -0.1 \
     -invalid_penalty -0.1 \
     -num_support 3 \
+    -no_agent \
     -wandb_enabled
