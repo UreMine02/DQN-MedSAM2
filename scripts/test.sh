f72c97d3a99ee82e48493d021660d9aa53d6c0e5
danh-real <htduyanh210604@gmail.com>
2026-01-09 23:19:06 +0000

    rl

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,9 +16,9 @@
 export CUDA_VISIBLE_DEVICES=0
 
 python eval_3d.py \
-    -pretrain output/msd_task02+grpo+icl/2026-01-09-11-23-34/best.pth \
+    -pretrain output/msd_task10+grpo+icl/2026-01-09-13-21-21/best.pth \
     -rl_config rl_modules/config/grpo_po_agent.yaml \
     -dataset msd \
-    -task Task02 \
+    -task Task10 \
     -data_path /data/datasets/nii/MSD \
     -num_support 5 \
