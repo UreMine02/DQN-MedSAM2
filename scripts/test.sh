a05ce828cb7f46a9f8a05413dfa894560a7d250b
danh-real <htduyanh210604@gmail.com>
2026-01-20 07:56:20 +0000

    msd01

--- scripts/test.sh
+++ scripts/test.sh
@@ -77,7 +77,7 @@ do
             -rl_config rl_modules/config/grpo_po_agent.yaml \
             -dataset msd \
             -task Task01 \
-            -data_path /data/datasets/nii/MSD \
+            -data_path /data/rlsam2/datasets/nii/MSD \
             -num_support $shot
             # -no_agent
             # -vis
