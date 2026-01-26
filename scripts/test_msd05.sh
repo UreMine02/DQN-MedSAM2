7db0b56fc736c96138c5c1766094761f66fc7680
danh-real <htduyanh210604@gmail.com>
2026-01-26 05:02:23 +0000

    ablation

--- scripts/test_msd05.sh
+++ scripts/test_msd05.sh
@@ -14,61 +14,61 @@
 # conda activate rlsam2
 
 ckpt=(
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_0_dice0.2672.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_1_dice0.4155.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_2_dice0.5166.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_3_dice0.5055.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_4_dice0.5374.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_5_dice0.4379.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_6_dice0.4816.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_7_dice0.4566.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_8_dice0.5189.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_9_dice0.5850.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_10_dice0.5693.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_11_dice0.5605.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_12_dice0.5999.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_13_dice0.5738.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_14_dice0.5901.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_15_dice0.6269.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_16_dice0.6204.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_17_dice0.6879.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_18_dice0.5837.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_19_dice0.6231.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_20_dice0.5965.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_21_dice0.5409.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_22_dice0.5839.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_23_dice0.6041.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_24_dice0.6417.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_25_dice0.5105.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_26_dice0.6117.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_27_dice0.6589.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_28_dice0.6063.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_29_dice0.6078.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_30_dice0.6786.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_31_dice0.6570.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_32_dice0.5632.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_33_dice0.6141.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_34_dice0.6814.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_35_dice0.5719.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_36_dice0.6353.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_37_dice0.6086.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_38_dice0.5964.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_39_dice0.6743.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_40_dice0.6127.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_41_dice0.6549.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_42_dice0.6517.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_43_dice0.6642.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_44_dice0.6088.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_45_dice0.6788.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_46_dice0.6955.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_47_dice0.6753.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_48_dice0.6397.pth
-    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-08-44-18/epoch_49_dice0.6280.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_0_dice0.3982.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_1_dice0.4674.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_2_dice0.5436.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_3_dice0.5741.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_4_dice0.5366.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_5_dice0.4406.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_6_dice0.5081.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_7_dice0.5754.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_8_dice0.5067.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_9_dice0.5753.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_10_dice0.5922.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_11_dice0.5285.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_12_dice0.5140.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_13_dice0.5583.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_14_dice0.5917.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_15_dice0.5465.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_16_dice0.6102.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_17_dice0.5695.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_18_dice0.4755.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_19_dice0.5631.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_20_dice0.5840.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_21_dice0.6573.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_22_dice0.6376.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_23_dice0.5969.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_24_dice0.6150.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_25_dice0.6720.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_26_dice0.6915.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_27_dice0.6358.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_28_dice0.5483.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_29_dice0.5937.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_30_dice0.5799.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_31_dice0.6589.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_32_dice0.5299.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_33_dice0.5682.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_34_dice0.6295.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_35_dice0.5695.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_36_dice0.6117.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_37_dice0.5610.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_38_dice0.7043.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_39_dice0.6423.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_40_dice0.6773.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_41_dice0.6757.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_42_dice0.6509.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_43_dice0.5614.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_44_dice0.6592.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_45_dice0.5867.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_46_dice0.6866.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_47_dice0.5762.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_48_dice0.5954.pth
+    /data/rlsam2/msd05/DQN-MedSAM2/output/msd_task05+grpo+icl/2026-01-26-12-31-21/epoch_49_dice0.6215.pth
 )
 
 for pretrain in ${ckpt[@]};
 do
-    for shot in 1 5;
+    for shot in 5;
     do
         python eval_3d.py \
             -pretrain $pretrain \
