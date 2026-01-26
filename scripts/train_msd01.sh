ef532d0d286dbc6c635eb7b283c5bcca31154ae9
danh-real <htduyanh210604@gmail.com>
2026-01-26 21:56:59 +0000

    msd

--- scripts/train_msd01.sh
+++ scripts/train_msd01.sh
@@ -27,7 +27,7 @@ python train_3d.py \
     -data_path /data/rlsam2/datasets/nii/MSD \
     -lr 1e-4 \
     -val_freq 1 \
-    -ep 50 \
+    -ep 5 \
     -q_updates_per_step 2 \
     -lazy_penalty -0.01 \
     -invalid_penalty -0.01 \
