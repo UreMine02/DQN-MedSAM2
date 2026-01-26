402870376b7273f67177eb8d9a5682d95527dcb8
danh-real <htduyanh210604@gmail.com>
2026-01-26 21:56:29 +0000

    msd

--- scripts/train_msd01.sh
+++ scripts/train_msd01.sh
@@ -14,7 +14,7 @@
 # conda init
 # conda activate rlsam2
 
-EXP=msd_task01+grpo+icl
+EXP=msd_task01+no_agent+icl
 # export CUDA_VISIBLE_DEVICES=0
 
 python train_3d.py \
@@ -32,4 +32,5 @@ python train_3d.py \
     -lazy_penalty -0.01 \
     -invalid_penalty -0.01 \
     -num_support 5 \
+    -no_agent \
     -distributed
