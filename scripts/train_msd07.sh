3cac91ffd65ff5fcf8383b555411dff74529e7f1
danh-real <htduyanh210604@gmail.com>
2026-01-28 01:42:06 +0000

    msd

--- scripts/train_msd07.sh
+++ scripts/train_msd07.sh
@@ -28,7 +28,7 @@ python train_3d.py \
     -lr 1e-4 \
     -val_freq 1 \
     -ep 50 \
-    -q_updates_per_step 2 \
+    -q_updates_per_step 1 \
     -lazy_penalty -0.01 \
     -invalid_penalty -0.01 \
     -num_support 5 \
