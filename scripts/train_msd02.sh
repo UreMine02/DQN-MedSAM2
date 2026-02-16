91ec9b5e3c9e4772fa8b12848d1cfd4509325b3a
danh-real <htduyanh210604@gmail.com>
2026-02-16 08:47:12 +0000

    msd

--- scripts/train_msd02.sh
+++ scripts/train_msd02.sh
@@ -14,23 +14,22 @@
 # conda init
 # conda activate rlsam2
 
-EXP=msd_task02+no_agent+icl+freeze
-export CUDA_VISIBLE_DEVICES=1
+EXP=msd_task02+ppo+icl
+# export CUDA_VISIBLE_DEVICES=1
 
 python train_3d.py \
     -exp_name $EXP \
     -sam_ckpt ./checkpoints/sam2_hiera_tiny.pt \
-    -rl_config rl_modules/config/grpo_po_agent.yaml \
+    -rl_config rl_modules/config/ppo_po_agent.yaml \
     -checkpoint_path ./output/$EXP \
     -dataset msd \
     -task Task02 \
     -data_path /data/datasets/nii/MSD \
-    -lr 2e-5 \
+    -lr 2e-4 \
     -val_freq 1 \
     -ep 50 \
     -q_updates_per_step 2 \
     -lazy_penalty -0.01 \
     -invalid_penalty -0.01 \
     -num_support 5 \
-    -no_agent \
-    -wandb_enabled
+    -distributed
