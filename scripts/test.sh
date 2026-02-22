8f2b76f3feeb2f38894a52d1d437ef5109c197a8
danh-real <htduyanh210604@gmail.com>
2026-02-22 07:32:18 +0000

    ablation

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,6 +16,7 @@
 ckpt=(
     output/msd_task02+grpo+icl/2026-02-22-10-36-54/best.pth
     # output/msd_task02+no_agent+icl+freeze/2026-02-19-20-33-41/best.pth
+    # output/msd_task02+grpo+icl+agent_only/2026-02-22-15-36-52/best.pth
 )
 
 export CUDA_VISIBLE_DEVICES=1
@@ -33,6 +34,6 @@ do
             -num_support $shot \
             -ablation \
             -vis \
-            # -no_agent
+            -no_agent
     done
 done
