86afd36a450761e3258a1aef972074ef6cb74a37
danh-real <htduyanh210604@gmail.com>
2026-02-26 08:46:34 +0000

    ablation

--- sam2_train/sam2_video_predictor.py
+++ sam2_train/sam2_video_predictor.py
@@ -1746,21 +1746,23 @@ class SAM2VideoPredictor(SAM2Base):
         drop_frame = None
         reward = 0.0
         action = action_out['main_action']
-        if "drop_frame" in output_dict.keys():
-            output_dict["drop_frame"][frame_idx] = -1
 
         if action == 0:
             # Add
             output_dict["non_cond_frame_outputs"][frame_idx-1] = output_dict["await_outputs"][frame_idx-1]
+            if "drop_frame" in output_dict.keys():
+                output_dict["drop_frame"][frame_idx] = -1
         elif action == 1:
             # Skip (equivalent to adding then drop the same frame)
             drop_frame = frame_idx - 1
             reward = 0.0
+            if "drop_frame" in output_dict.keys():
+                output_dict["drop_frame"][frame_idx] = frame_idx-1
         else:
             # Add the new frame and skip a specific frame
             drop_frame = action_frame_map[action]
             if "drop_frame" in output_dict.keys():
-                output_dict["drop_frame"][frame_idx] = list(output_dict["non_cond_frame_outputs"].keys()).index(drop_frame)
+                output_dict["drop_frame"][frame_idx] = drop_frame
             output_dict["non_cond_frame_outputs"].pop(drop_frame)
             output_dict["non_cond_frame_outputs"][frame_idx-1] = output_dict["await_outputs"][frame_idx-1]
 
@@ -1899,7 +1901,8 @@ class SAM2VideoPredictor(SAM2Base):
         output_dict,
         agent_act,
         **kwargs):
-
+        
+        output_dict["dice_drop"][frame_idx] = {}
         video_H = inference_state["video_height"]
         video_W = inference_state["video_width"]
 
@@ -1927,10 +1930,10 @@ class SAM2VideoPredictor(SAM2Base):
             )
         pred_masks = (pred_masks.sigmoid() > 0.5).float()
 
-        dice_before = dice_score(pred_masks, gt_masks, smoothing=1e-8)
+        dice_before = dice_score(pred_masks, gt_masks, smoothing=1e-8) # Drop i-1
+        output_dict["dice_drop"][frame_idx][frame_idx - 1] = dice_before.item()
 
-        output_dict["dice_drop"][frame_idx] = {}
-        for prev_frame_idx in output_dict["non_cond_frame_outputs"].keys():
+        for i, prev_frame_idx in enumerate(output_dict["non_cond_frame_outputs"].keys()):
             temp_output_dict = {
                 "cond_frame_outputs": output_dict["cond_frame_outputs"].copy(),
                 "non_cond_frame_outputs": output_dict["non_cond_frame_outputs"].copy()
@@ -1963,15 +1966,14 @@ class SAM2VideoPredictor(SAM2Base):
             pred_masks = (pred_masks.sigmoid() > 0.5).float()
 
             dice_after = dice_score(pred_masks, gt_masks, smoothing=1e-8)
-
-            output_dict["dice_drop"][frame_idx][prev_frame_idx] = (dice_after - dice_before).item()
+            output_dict["dice_drop"][frame_idx][prev_frame_idx] = dice_after.item()
 
         if not agent_act:
             output_dict["drop_frame"][frame_idx] = -1
             if len(output_dict["dice_drop"][frame_idx]) == self.num_maskmem - 1:
                 drop_frame = list(output_dict["non_cond_frame_outputs"].keys())[0]
                 output_dict["non_cond_frame_outputs"].pop(drop_frame)
-                output_dict["drop_frame"][frame_idx] = 0
+                output_dict["drop_frame"][frame_idx] = frame_idx - 1
 
             if frame_idx > 0:
                 output_dict["non_cond_frame_outputs"][frame_idx-1] = output_dict["await_outputs"][frame_idx-1]
--- scripts/test.sh
+++ scripts/test.sh
@@ -15,10 +15,10 @@
 
 ckpt=(
     # output/msd_task02+grpo+icl/2026-02-22-10-36-54/best.pth
-    # output/msd_task02+no_agent+icl+freeze/2026-02-19-20-33-41/best.pth
+    output/msd_task02+no_agent+icl+freeze/2026-02-19-20-33-41/best.pth
     # output/msd_task02+grpo+icl+agent_only/2026-02-22-15-36-52/best.pth
 
-    output/msd_task02+grpo+icl/2026-02-26-12-37-46/best.pth
+    # output/msd_task02+grpo+icl/2026-02-26-12-37-46/best.pth
 )
 
 export CUDA_VISIBLE_DEVICES=0
@@ -36,6 +36,6 @@ do
             -num_support $shot \
             -ablation \
             -vis \
-            # -no_agent
+            -no_agent
     done
 done
