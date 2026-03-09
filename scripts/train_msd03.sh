6343f0ec4a043531bd823e874050f42e67f44687
danh-real <htduyanh210604@gmail.com>
2026-03-09 10:17:55 +0000

    msd

--- scripts/train_msd03.sh
+++ scripts/train_msd03.sh
@@ -15,7 +15,7 @@
 # conda activate rlsam2
 
 EXP=msd_task03+grpo+icl
-export CUDA_VISIBLE_DEVICES=0
+# export CUDA_VISIBLE_DEVICES=0
 
 python train_3d.py \
     -exp_name $EXP \
@@ -27,9 +27,10 @@ python train_3d.py \
     -data_path /data/rlsam2/datasets/nii/MSD \
     -lr 1e-4 \
     -val_freq 1 \
-    -ep 50 \
+    -ep 100 \
     -q_updates_per_step 5 \
     -lazy_penalty 0.0 \
     -invalid_penalty -0.01 \
     -num_support 3 \
-    -wandb_enabled
+    -wandb_enabled \
+    -distributed
