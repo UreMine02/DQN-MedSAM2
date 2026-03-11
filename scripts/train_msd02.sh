eb77e6d9329f0bde2ada49fd95ee6868187bed90
danh-real <htduyanh210604@gmail.com>
2026-03-11 05:11:16 +0000

    msd

--- scripts/train_msd02.sh
+++ scripts/train_msd02.sh
@@ -28,7 +28,7 @@ python train_3d.py \
     -data_path /data/datasets/nii/MSD \
     -lr 1e-4 \
     -val_freq 1 \
-    -ep 100 \
+    -ep 500 \
     -q_updates_per_step 5 \
     -lazy_penalty 0.0 \
     -invalid_penalty -0.01 \
--- scripts/train_msd10.sh
+++ scripts/train_msd10.sh
@@ -9,26 +9,28 @@
 #SBATCH -A strategic
 #SBATCH -o "/hpcfs/users/a1232079/duyanh/MedSAM2/code/DQN-MedSAM2/msd10-%j.out"
 
-conda activate rlsam2
-cd /hpcfs/users/a1232079/duyanh/MedSAM2/code/DQN-MedSAM2
-conda init
-conda activate rlsam2
+# conda activate rlsam2
+# cd /hpcfs/users/a1232079/duyanh/MedSAM2/code/DQN-MedSAM2
+# conda init
+# conda activate rlsam2
 
 EXP=msd_task10+grpo+icl
-export CUDA_VISIBLE_DEVICES=1
+# export CUDA_VISIBLE_DEVICES=1
 
 python train_3d.py \
     -exp_name $EXP \
-    -sam_ckpt ./checkpoints/sam2_hiera_tiny.pt \
+    -sam_ckpt /data/rlsam2/checkpoints/sam2_hiera_tiny.pt \
     -rl_config rl_modules/config/grpo_po_agent.yaml \
     -checkpoint_path ./output/$EXP \
     -dataset msd \
     -task Task10 \
-    -data_path /hpcfs/users/a1232079/duyanh/MedSAM2/datasets/nii/MSD \
+    -data_path /data/rlsam2/datasets/nii/MSD \
     -lr 1e-4 \
     -val_freq 1 \
     -ep 100 \
     -q_updates_per_step 5 \
     -lazy_penalty 0.0 \
     -invalid_penalty -0.01 \
-    -num_support 3
+    -num_support 3 \
+    -distributed \
+    -wandb_enabled
