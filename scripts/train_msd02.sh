0dc0d4831fcd90a530f7215a4d56e06df846e481
danh-real <htduyanh210604@gmail.com>
2026-03-13 08:28:10 +0000

    msd01

--- scripts/train_msd02.sh
+++ scripts/train_msd02.sh
@@ -14,8 +14,8 @@
 # conda init
 # conda activate rlsam2
 
-EXP=msd_task02+grpo+icl+no_pos_embed
-export CUDA_VISIBLE_DEVICES=0
+EXP=msd_task02+grpo+icl+inclue_obj_ptr_from_cond
+export CUDA_VISIBLE_DEVICES=1
 
 python train_3d.py \
     -exp_name $EXP \
@@ -32,5 +32,5 @@ python train_3d.py \
     -q_updates_per_step 1 \
     -lazy_penalty 0.0 \
     -invalid_penalty -0.01 \
-    -num_support 10 \
+    -num_support 3 \
     -wandb_enabled
