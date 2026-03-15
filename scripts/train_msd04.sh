5e47588806c29c9562aff172fb182052a4c73306
danh-real <htduyanh210604@gmail.com>
2026-03-15 01:19:27 +0000

    multiobj

--- scripts/train_msd04.sh
+++ scripts/train_msd04.sh
@@ -32,5 +32,5 @@ python train_3d.py \
     -lazy_penalty 0.0 \
     -invalid_penalty -0.01 \
     -num_support 5 \
-    # -distributed \
-    # -wandb_enabled
+    -distributed \
+    -wandb_enabled
