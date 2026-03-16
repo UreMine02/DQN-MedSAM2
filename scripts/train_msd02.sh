e98898ce975f4edcd422f41b32e4e26b7ccf12b9
danh-real <htduyanh210604@gmail.com>
2026-03-16 03:11:46 +0000

    msd

--- scripts/train_msd02.sh
+++ scripts/train_msd02.sh
@@ -14,7 +14,7 @@
 # conda init
 # conda activate rlsam2
 
-EXP=msd_task02+grpo+icl+test
+EXP=msd_task02+grpo+icl+cw_gating+obj_ptr_semantic_filtering
 export CUDA_VISIBLE_DEVICES=0
 
 python train_3d.py \
@@ -33,4 +33,4 @@ python train_3d.py \
     -lazy_penalty 0.0 \
     -invalid_penalty -0.01 \
     -num_support 5 \
-    # -wandb_enabled
+    -wandb_enabled
