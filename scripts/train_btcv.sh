9b1372229cb16b6eebabae14a61e43fd4fda0a30
danh-real <htduyanh210604@gmail.com>
2026-03-19 00:05:13 +0000

    msd

--- scripts/train_btcv.sh
+++ scripts/train_btcv.sh
@@ -9,25 +9,25 @@
 #SBATCH -A strategic
 #SBATCH -o "/hpcfs/users/a1232079/duyanh/MedSAM2/code/DQN-MedSAM2/btcv-%j.out"
 
-conda activate rlsam2
-cd /hpcfs/users/a1232079/duyanh/MedSAM2/code/DQN-MedSAM2
-conda init
-conda activate rlsam2
+# conda activate rlsam2
+# cd /hpcfs/users/a1232079/duyanh/MedSAM2/code/DQN-MedSAM2
+# conda init
+# conda activate rlsam2
 
-EXP=btcv+grpo+icl
+EXP=btcv+grpo+icl+sw_gating+semantic_filtering_with_proj_before_reshape
 
 python train_3d.py \
     -exp_name $EXP \
-    -sam_ckpt ./checkpoints/sam2_hiera_tiny.pt \
+    -sam_ckpt /data/rlsam2/checkpoints/sam2_hiera_tiny.pt \
     -rl_config rl_modules/config/grpo_po_agent.yaml \
     -checkpoint_path ./output/$EXP \
     -dataset btcv \
-    -data_path /hpcfs/users/a1232079/duyanh/MedSAM2/datasets/nii/BTCV \
-    -lr 1e-4 \
+    -data_path /data/rlsam2/datasets/nii/BTCV \
+    -lr 2e-4 \
     -val_freq 1 \
     -ep 100 \
     -q_updates_per_step 1 \
     -lazy_penalty 0.0 \
     -invalid_penalty -0.01 \
-    -num_support 3 \
+    -num_support 5 \
     -distributed
