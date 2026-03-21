59c7f902ddab6a6f1296405267fb61a58ac59af1
danh-real <htduyanh210604@gmail.com>
2026-03-21 05:26:26 +0000

    msd

--- func_3d/dataset/msd.py
+++ func_3d/dataset/msd.py
@@ -171,7 +171,7 @@ class MSD(Dataset):
         
         if image_3d.shape[-1] > max_slices and max_slices > 0:
             if slice_selection == 'contiguous':
-                choices = list(range(-(max_slices-1),0)) + list(range(image_3d.shape[-1]))
+                choices = list(range(-(max_slices-2),0)) + list(range(image_3d.shape[-1] - 1))
                 start = np.random.choice(choices)
                 end = start + max_slices
                 start = max(0, start)
