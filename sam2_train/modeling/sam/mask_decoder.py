e113c746169493f4f5885205bac0d5228d4240f5
danh-real <htduyanh210604@gmail.com>
2026-04-13 04:26:59 +0000

    msd

--- sam2_train/modeling/sam/mask_decoder.py
+++ sam2_train/modeling/sam/mask_decoder.py
@@ -212,6 +212,9 @@ class MaskDecoder(nn.Module):
         # Run the transformer
         # hs: processed point_embedding | src: processed image_embedding
         # hs, src = checkpoint(self.transformer, src, pos_src, tokens, use_reentrant=False) # (torch.Size([1, 8, 256]), torch.Size([1, 4096, 256]))
+        assert not tokens.isnan().any()
+        assert not src.isnan().any()
+        assert not pos_src.isnan().any()
         hs, src = self.transformer(src, pos_src, tokens)
         assert not hs.isnan().any()
         iou_token_out = hs[:, s, :] # torch.Size([1, 256]
--- scripts/train_msd02.sh
+++ scripts/train_msd02.sh
@@ -14,8 +14,8 @@
 # conda init
 # conda activate rlsam2
 
-EXP=msd_task02+icl+ppo+entropy1e-5+long_horizon+no_augment
-export CUDA_VISIBLE_DEVICES=0
+EXP=msd_task02+icl+ppo+long_horizon+no_augment
+export CUDA_VISIBLE_DEVICES=1
 
 python train_3d.py \
     -exp_name $EXP \
@@ -26,7 +26,7 @@ python train_3d.py \
     -dataset msd \
     -task Task02 \
     -data_path /data/datasets/nii/MSD \
-    -lr 2e-4 \
+    -lr 1e-4 \
     -val_freq 1 \
     -ep 100 \
     -q_updates_per_step 5 \
@@ -34,8 +34,9 @@ python train_3d.py \
     -invalid_penalty -0.01 \
     -num_support 5 \
     -memory_bank_size 6 \
-    -gating_dimension "no" \
+    -gating_dimension "cw" \
     -gating_softness "soft" \
     -auxiliary_loss "no" \
     -wandb_enabled \
-    # -obj_ptr_gating \
+    -obj_ptr_gating \
+    -highres_gating "by_lowres"
