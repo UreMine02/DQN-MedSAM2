096113dda6a57b0799aa452a98da4a0d0ca5e19f
danh-real <htduyanh210604@gmail.com>
2026-04-13 00:53:54 +0000

    msd

--- func_3d/function.py
+++ func_3d/function.py
@@ -30,7 +30,7 @@ import wandb
 args = cfg.parse_args()
 
 GPUdevice = torch.device('cuda', args.gpu_device)
-paper_loss = CombinedLoss(focal_weight=10, dice_weight=1)
+paper_loss = CombinedLoss(focal_weight=1, dice_weight=1)
 seed = torch.randint(1,11,(1,7))
 
 torch.backends.cudnn.benchmark = True
