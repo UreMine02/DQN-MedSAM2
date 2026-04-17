42a4b988494b35793187e2bfe815630784c6c0a8
danh-real <htduyanh210604@gmail.com>
2026-04-17 09:12:35 +0000

    msd

--- scripts/train_msd10.sh
+++ scripts/train_msd10.sh
@@ -15,7 +15,7 @@
 # conda activate rlsam2
 
 EXP=msd_task10+icl+ppo+long_horizon+no_augment
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 python train_3d.py \
     -exp_name $EXP \
@@ -25,7 +25,7 @@ python train_3d.py \
     -dataset msd \
     -task Task10 \
     -data_path /data/datasets/nii/MSD \
-    -lr 1e-4 \
+    -lr 2e-4 \
     -val_freq 1 \
     -ep 100 \
     -q_updates_per_step 5 \
--- train_3d.py
+++ train_3d.py
@@ -72,12 +72,19 @@ def train(rank=0, world_size=0):
 
     # if not args.no_agent:
     for name, param in net.named_parameters():
-        if "image_encoder" in name:
-            param.requires_grad_(False)
-        elif "sam_prompt_encoder" in name:
-            param.requires_grad_(False)
-        else:
+        if "memory_attention" in name:
             param.requires_grad_(True)
+        elif "maskmem_tpos_enc" in name:
+            param.requires_grad_(True)
+        else:
+            param.requires_grad_(False)
+        
+        # if "image_encoder" in name:
+        #     param.requires_grad_(False)
+        # elif "sam_prompt_encoder" in name:
+        #     param.requires_grad_(False)
+        # else:
+        #     param.requires_grad_(True)
 
     agent_n_params = 0
     if agent is not None:
