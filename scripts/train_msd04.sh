aa28b3482bc27f1aa9a9e2bed235324254e8ba7b
danh-real <htduyanh210604@gmail.com>
2026-04-26 07:23:32 +0000

    msd

--- scripts/train_msd04.sh
+++ scripts/train_msd04.sh
@@ -14,22 +14,23 @@ cd /hpcfs/users/a1232079/duyanh/MedSAM2/code/DQN-MedSAM2
 conda init
 conda activate rlsam2
 
-EXP=msd_task04+grpo+icl+cw_gating_before_pos+semantic_filtering+equal_prob+augment
+EXP=msd_task04+grpo+icl+long_horizon+no_augment
 
 python train_3d.py \
     -exp_name $EXP \
-    -sam_ckpt ./checkpoints/sam2_hiera_tiny.pt \
+    -sam_ckpt /data/rlsam2/checkpoints/sam2_hiera_tiny.pt \
     -rl_config rl_modules/config/grpo_po_agent.yaml \
     -checkpoint_path ./output/$EXP \
     -dataset msd \
     -task Task04 \
-    -data_path /hpcfs/users/a1232079/duyanh/MedSAM2/datasets/nii/MSD \
-    -lr 2e-4 \
+    -data_path /data/rlsam2/datasets/nii/ \
+    -lr 1e-4 \
     -val_freq 1 \
-    -ep 500 \
+    -ep 100 \
     -q_updates_per_step 1 \
     -lazy_penalty 0.0 \
     -invalid_penalty -0.01 \
     -num_support 5 \
+    -memory_bank_size 6 \
     -distributed \
     -wandb_enabled
