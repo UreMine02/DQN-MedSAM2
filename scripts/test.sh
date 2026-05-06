2b08b09b0ab32b4e3ef1ecff49a8b3d784189ebf
danh-real <htduyanh210604@gmail.com>
2026-05-06 09:52:25 +0000

    msd

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,11 +16,12 @@
 # conda activate rlsam2
 
 declare -a ckpt=(
-    # output/msd_task02+icl+grpo+raw_dice+long_horizon+no_augment/2026-05-04-11-50-16/best.pth
+    output/msd_task02+icl+no_agent+long_horizon+no_augment/2026-04-15-19-42-29/best.pth
+    # output/msd_task02+icl+grpo+raw_dice+long_horizon+no_augment/2026-05-06-10-30-25/best.pth
     # output/msd_task02+icl+random_drop+long_horizon+flip_augment/2026-04-24-12-59-55/best.pth
     # output/msd_task02+icl+grpo+penalty0.0+long_horizon+no_augment/2026-05-05-14-52-15/best.pth
     # output/msd_task02+icl+grpo+long_horizon+no_augment+lr2e-4/2026-05-01-07-12-31/best.pth
-    output/msd_task02+icl+grpo+lazy_pen0.1+long_horizon+flip_augment/2026-04-23-09-52-39/best.pth
+    # output/msd_task02+icl+grpo+lazy_pen0.1+long_horizon+flip_augment/2026-04-23-09-52-39/best.pth
     # output/msd_task02+icl+grpo+lazy_pen1.0+long_horizon+flip_augment/2026-04-23-16-01-08/best.pth
 )
 
@@ -38,7 +39,7 @@ do
             -data_path /data/datasets/nii/ \
             -num_support $shot \
             -memory_bank_size 6 \
-            # -no_agent \
+            -no_agent \
             # -random_drop
     done
 done
