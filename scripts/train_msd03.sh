8c89667a41d723f68e57895ecf0d527dfb8b186e
danh-real <htduyanh210604@gmail.com>
2026-09-19 09:39:42 +0000

    grpo

--- scripts/train_msd03.sh
+++ scripts/train_msd03.sh
@@ -24,7 +24,7 @@ python train_3d.py \
     -checkpoint_path ./output/$EXP \
     -dataset msd \
     -task "Task03" \
-    -data_path /data/rlsam2/datasets/nii/MSD \
+    -data_path /data/rlsam2/datasets/nii/ \
     -lr 2e-4 \
     -val_freq 1 \
     -ep 50 \
